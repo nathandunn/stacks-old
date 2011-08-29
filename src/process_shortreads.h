31fe1df9a8dbc40454dea22d77fcb0d93a1eeb39
catchen <catchen@localhost>
2011-08-29 21:20:24 +0000

    Initial iteration of process_shortreads complete

--- src/process_shortreads.h
+++ src/process_shortreads.h
@@ -48,6 +48,7 @@ using std::set;
 using std::pair;
 
 #include "constants.h" 
+#include "clean.h"
 #include "Bustard.h"   // Reading input files in Tab-separated Bustard format
 #include "Fastq.h"     // Reading input files in FASTQ format
 
@@ -56,25 +57,6 @@ struct eqstr {
 	return strcmp(s1, s2) == 0;
     }
 };
-
-typedef struct read {
-    char   *barcode;
-    char   *machine;
-    int     lane;
-    int     tile;
-    int     x;
-    int     y;
-    int     index;
-    int     read;
-    char   *seq;
-    char   *phred;
-    int    *int_scores;
-    int     filter;
-    int     retain;
-    unsigned int len;
-    double  win_len;
-    double  stop_pos;
-} Read;
 
 void help( void );
 void version( void );
@@ -90,8 +72,6 @@ int  parse_input_record(Seq *, Read *);
 int  correct_barcode(map<string, ofstream *> &, Read *, map<string, long> &, map<string, map<string, long> > &);
 int  check_quality_scores(Read *, bool);
 int  dist(const char *, char *);
-int  write_fastq(map<string, ofstream *> &, Read *, bool);
-int  write_fasta(map<string, ofstream *> &, Read *, bool);
 int  print_results(vector<string> &, map<string, map<string, long> > &, map<string, map<string, long> > &);
 
 int  compare_barcodes(pair<string, int>, pair<string, int>);
