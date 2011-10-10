2a805dec2fe6df31ad2877f67ef6c8078f6cde6d
catchen <catchen@localhost>
2011-10-10 06:09:42 +0000

    Merging to trunk

---
+++ src/clean.h
@@ -0,0 +1,66 @@
+// -*-mode:c++; c-style:k&r; c-basic-offset:4;-*-
+//
+// Copyright 2011, Julian Catchen <jcatchen@uoregon.edu>
+//
+// This file is part of Stacks.
+//
+// Stacks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Stacks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Stacks.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __CLEAN_H__
+#define __CLEAN_H__
+
+#include <string>
+using std::string;
+#include <map>
+using std::map;
+#include <iostream>
+#include <fstream>
+using std::ofstream;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#include "input.h"
+
+typedef struct read {
+    char   *barcode;
+    char   *machine;
+    int     lane;
+    int     tile;
+    int     x;
+    int     y;
+    int     index;
+    int     read;
+    char   *seq;
+    char   *phred;
+    int    *int_scores;
+    int     filter;
+    int     retain;
+    unsigned int len;
+    double  win_len;
+    double  stop_pos;
+} Read;
+
+extern int barcode_size;
+
+int  parse_illumina_v1(const char *);
+int  parse_illumina_v2(const char *);
+int  write_fastq(map<string, ofstream *> &, Read *, bool);
+int  write_fastq(ofstream *, Seq *);
+int  write_fasta(map<string, ofstream *> &, Read *, bool);
+int  write_fasta(ofstream *, Seq *);
+
+#endif // __CLEAN_H__
---
+++ stacks.sql
@@ -0,0 +1,197 @@
+create table batches (
+       id           int unsigned not null primary key auto_increment,
+       date         DATE not null,
+       description  tinytext
+);
+
+create table samples (
+       id        int unsigned not null primary key auto_increment,
+       batch_id	 int unsigned not null,
+       sample_id int unsigned not null,
+       type      enum('parent', 'progeny', 'sample'),
+       file      varchar(128)
+);
+
+create table catalog_tags (
+       id    	    int unsigned not null primary key auto_increment,
+       batch_id     int unsigned not null,
+       tag_id	    int unsigned not null,
+       chr          varchar(32),
+       bp           int unsigned default 0,
+       relationship enum('consensus', 'primary', 'secondary', 'tertiary'),
+       sub_id	    int unsigned not null,
+       merge_type   tinytext,
+       seq 	    tinytext,
+       INDEX        batch_id_index (batch_id),
+       INDEX        tag_id_index (tag_id)
+);
+
+create table catalog_snps (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       tag_id      int unsigned not null,
+       col         int unsigned not null,
+       lratio      float,
+       rank_1	   char(1),
+       rank_2	   char(1),
+       INDEX       batch_index (batch_id),
+       INDEX       tag_index (tag_id)
+);
+
+create table catalog_alleles (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       tag_id      int unsigned not null,
+       allele      varchar(32),
+       read_pct    float,
+       read_cnt    int unsigned,
+       INDEX       batch_index (batch_id),
+       INDEX       tag_index (tag_id)
+);
+
+create table catalog_genotypes (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       catalog_id  int unsigned not null,
+       sample_id   int unsigned not null,
+       genotype    char(2),
+       INDEX       batch_index (batch_id),
+       INDEX       catalog_index (catalog_id),
+       INDEX       sample_index (sample_id)
+);
+
+create table genotype_corrections (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       catalog_id  int unsigned not null,
+       sample_id   int unsigned not null,
+       genotype    char(2),
+       INDEX       cat_index (catalog_id),
+       INDEX       batch_index (batch_id),
+       INDEX       sample_index (sample_id)
+);
+
+create table catalog_annotations (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       catalog_id  int unsigned not null,
+       external_id varchar(64),
+       INDEX       batch_index (batch_id),
+       INDEX       catalog_index (catalog_id),
+       INDEX       external_index (external_id)
+);
+
+create table pileup (
+       id    	    int unsigned not null primary key auto_increment,
+       sample_id    int unsigned not null,
+       tag_id	    int unsigned not null,
+       chr          varchar(32),
+       bp           int unsigned default 0,
+       relationship enum('consensus', 'primary', 'secondary', 'tertiary'),
+       sub_id	    int unsigned not null,
+       seq_id	    varchar(32),
+       seq 	    text,
+       INDEX        tag_id_index (tag_id),
+       INDEX        sample_id_index (sample_id),
+       INDEX        rel_index (relationship)
+);
+
+create table unique_tags (
+       id    	    int unsigned not null primary key auto_increment,
+       sample_id    int unsigned not null,
+       tag_id	    int unsigned not null,
+       chr          varchar(32),
+       bp           int unsigned default 0,
+       relationship enum('consensus', 'primary', 'secondary', 'tertiary'),
+       sub_id	    int unsigned not null,
+       seq_id	    varchar(32),
+       seq 	    tinytext,
+       deleveraged  bool default false,
+       blacklisted  bool default false,
+       removed      bool default false,
+       INDEX        tag_id_index (tag_id),
+       INDEX        sample_id_index (sample_id),
+       INDEX        rel_index (relationship)
+);
+
+create table snps (
+       id    	   int unsigned not null primary key auto_increment,
+       sample_id   int unsigned not null,
+       tag_id      int unsigned not null,
+       col         int unsigned not null,
+       lratio      float,
+       rank_1	   char(1),
+       rank_2	   char(1),
+       INDEX       samp_index (sample_id),
+       INDEX       tag_index (tag_id)
+);
+
+create table alleles (
+       id    	   int unsigned not null primary key auto_increment,
+       sample_id   int unsigned not null,
+       tag_id      int unsigned not null,
+       allele      varchar(32),
+       read_pct    float,
+       read_cnt    int unsigned,
+       INDEX       samp_index (sample_id),
+       INDEX       tag_index (tag_id)
+);
+
+create table matches (
+       id    	   int unsigned not null primary key auto_increment,
+       batch_id    int unsigned not null,
+       catalog_id  int unsigned not null,
+       sample_id   int unsigned not null,
+       tag_id      int unsigned not null,
+       allele	   varchar(32),
+       depth       int unsigned not null,
+       INDEX	   batch_id_index (batch_id),
+       INDEX	   catalog_id_index (catalog_id),
+       INDEX	   sample_id_index (sample_id),
+       INDEX	   tag_id_index (tag_id)
+);
+
+create table markers (
+       id         int unsigned not null primary key auto_increment,
+       batch_id   int unsigned not null,
+       catalog_id int unsigned not null,
+       type       enum('aa/bb', 'ab/--', '--/ab', 'aa/ab', 'ab/aa', 'ab/ab', 'ab/ac', 'ab/cd', 'ab/cc', 'cc/ab'),
+       progeny    int unsigned not null default 0,
+       max_pct    float,
+       ratio      varchar(128),
+       f          float
+);
+
+create table sequence (
+       id         int unsigned not null primary key auto_increment,
+       batch_id   int unsigned not null,
+       catalog_id int unsigned not null,
+       type       enum('pe_radtag', 'est'),
+       seq_id     varchar(64),
+       seq        text,
+       INDEX      catalog_id_index (catalog_id)
+);
+
+create table sequence_blast (
+       id               int unsigned not null primary key auto_increment,
+       batch_id         int unsigned not null default 0,
+       catalog_id       int unsigned not null default 0,
+       seq_id           int unsigned not null default 0,
+       algorithm        enum('blastn', 'blastp', 'blastx', 'tblastn', 'tblastx'),
+       query_id         varchar(64),
+       query_len        int unsigned not null default 0,
+       hit_id           varchar(128),
+       hit_len          int unsigned not null default 0,
+       score            double,
+       e_value          double,
+       percent_ident    double,
+       hsp_rank         int unsigned not null default 0,
+       aln_len          int unsigned not null default 0,
+       aln_homology_str text,
+       query_aln        text,
+       query_aln_start  int unsigned not null default 0,
+       query_aln_end    int unsigned not null default 0,
+       hit_aln          text,
+       hit_aln_start    int unsigned not null default 0,
+       hit_aln_end      int unsigned not null default 0
+);
