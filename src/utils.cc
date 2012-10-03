fb947c14625cdf5ecc31ba72a37da7078ef37913
catchen <catchen@localhost>
2012-10-03 22:51:28 +0000

    Stacks version 0.9999

---
+++ src/utils.cc
@@ -0,0 +1,100 @@
+// -*-mode:c++; c-style:k&r; c-basic-offset:4;-*-
+//
+// Copyright 2010, Julian Catchen <jcatchen@uoregon.edu>
+//
+// This file is part of Stacks.
+//
+// Stacks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Stacks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Stacks.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+// utils.cc -- common routines needed in multiple object files.
+//
+// Julian Catchen
+// jcatchen@uoregon.edu
+// University of Oregon
+//
+// $Id: utils.cc 1989 2010-11-03 01:00:23Z catchen $
+//
+#include "utils.h"
+
+double factorial(double n) {
+    double fact = 1;
+
+    for (double i = n; i > 1; i--)
+        fact *= i;
+
+    return fact;
+}
+
+double reduced_factorial(double n, double d) {
+    double f = n - d;
+
+    if (f < 0) 
+        return 0;
+    else if (f == 0) 
+        return 1;
+    else if (f == 1)
+        return n;
+
+    f = n;
+    n--;
+    while (n > d) {
+        f *= n;
+        n--;
+    }
+
+    return f;
+}
+
+double log_factorial(double n) {
+    double fact = 0;
+
+    for (double i = n; i > 1; i--)
+        fact += log(i);
+
+    return fact;
+}
+
+double reduced_log_factorial(double n, double d) {
+    double f = n - d;
+
+    if (f < 0) 
+        return 0;
+    else if (f == 0) 
+        return 0;
+    else if (f == 1)
+        return log(n);
+
+    f = log(n);
+    n--;
+    while (n > d) {
+        f += log(n);
+        n--;
+    }
+
+    return f;
+}
+
+bool compare_pair(pair<char, int> a, pair<char, int> b) {
+    return (a.second > b.second);
+}
+
+bool compare_pair_intdouble(pair<int, double> a, pair<int, double> b) {
+    return (a.second < b.second);
+}
+
+bool compare_ints(int a, int b) {
+    return (a > b);
+}
---
+++ src/utils.h
@@ -0,0 +1,59 @@
+// -*-mode:c++; c-style:k&r; c-basic-offset:4;-*-
+//
+// Copyright 2010, Julian Catchen <jcatchen@uoregon.edu>
+//
+// This file is part of Stacks.
+//
+// Stacks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Stacks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Stacks.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <math.h>
+#include <iostream>
+using std::cerr;
+using std::endl;
+#include <utility>
+using std::pair;
+using std::make_pair;
+
+double factorial(double);
+double reduced_factorial(double, double);
+
+double log_factorial(double);
+double reduced_log_factorial(double, double);
+//
+// Comparison functions for the STL sort routine
+//
+bool compare_ints(int, int);
+bool compare_pair(pair<char, int>, pair<char, int>);
+bool compare_pair_intdouble(pair<int, double>, pair<int, double>);
+
+//
+// Comparison classes for STL sets
+//
+struct int_increasing {
+    bool operator() (const int& lhs, const int& rhs) const {
+	return lhs < rhs;
+    }
+};
+
+struct int_decreasing {
+    bool operator() (const int& lhs, const int& rhs) const {
+	return lhs > rhs;
+    }
+};
+
+#endif // __UTILS_H__
