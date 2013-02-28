2ac7c1bbaf7b8ba01112f43a1b6294115e4145d7
Nathan Dunn <ndunn@me.com>
2013-02-27 18:36:08 -0800

    updated stacks

---
+++ MacOSXClient/StacksGUI2/StacksView.h
@@ -0,0 +1,15 @@
+//
+// Created by ndunn on 2/27/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+/**
+* This is just a raw read formatted:
+* http://genome.uoregon.edu/stacks/tag.php?db=tut_radtags&batch_id=1&sample_id=7&tag_id=408
+*/
+@interface StacksView : NSObject
+@end
