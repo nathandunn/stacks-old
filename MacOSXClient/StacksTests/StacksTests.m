29cd80db9192e0b743ed0649da044b89f6bea14f
Nathan Dunn <ndunn@me.com>
2013-03-13 14:33:03 -0700

    test added

---
+++ MacOSXClient/StacksTests/StacksTests.m
@@ -0,0 +1,33 @@
+//
+//  StacksTests.m
+//  StacksTests
+//
+//  Created by Nathan Dunn on 3/13/13.
+//  Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+
+#import "StacksTests.h"
+
+@implementation StacksTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testBob
+{
+//    STFail(@"Unit tests are not implemented yet in StacksTests");
+    STAssertNil(nil,@"should obviously be nil");
+}
+
+@end
