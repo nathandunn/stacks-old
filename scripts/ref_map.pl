3016c988eba4a537abf82cbe5f3cdaf42cf62c1d
Julian Catchen <julian.catchen@gmail.com>
2013-04-04 16:48:56 -0700

    Implemented AMOVA Fst and Jakobsson Fst to populations. Added NsiI to restriction enzymes.

--- scripts/ref_map.pl
+++ scripts/ref_map.pl
@@ -536,9 +536,9 @@ ref_map.pl -p path -r path [-s path] -o path [-n mismatches] [-m min_cov] [-T nu
     i: starting sample_id, this is determined automatically if database interaction is enabled.
 
    SNP Model Options (these options are passed on to pstacks):
-    --bound_low: lower bound for epsilon, the error rate, between 0 and 1.0 (default 0).
-    --bound_high: upper bound for epsilon, the error rate, between 0 and 1.0 (default 1).
-    --alpha: chi square significance level required to call a heterozygote or homozygote, either 0.1, 0.05 (default), 0.01, or 0.001.
+    --bound_low <num>: lower bound for epsilon, the error rate, between 0 and 1.0 (default 0).
+    --bound_high <num>: upper bound for epsilon, the error rate, between 0 and 1.0 (default 1).
+    --alpha <num>: chi square significance level required to call a heterozygote or homozygote, either 0.1, 0.05 (default), 0.01, or 0.001.
 
 EOQ
 
