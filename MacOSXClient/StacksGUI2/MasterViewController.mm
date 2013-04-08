b6dbde3bb11e4e3bd37a2d6c6d470889ef85ad8c
Nathan Dunn <ndunn@uoregon.edu>
2013-04-08 15:57:15 -0700

    some refactoring

--- MacOSXClient/StacksGUI2/MasterViewController.mm
+++ MacOSXClient/StacksGUI2/MasterViewController.mm
@@ -22,11 +22,8 @@
 
 @interface MasterViewController ()
 
-@property(weak) IBOutlet NSTableView *genotypeTableView;
 @property(weak) IBOutlet NSTableView *stacksTableView;
 @property(weak) IBOutlet NSTableView *locusTableView;
-//@property(weak) IBOutlet NSTextField *locusDetail;
-//@property(weak) IBOutlet NSTextField *consensusDetail;
 @property(strong) IBOutlet NSWindow *mainWindow;
 
 @end
@@ -41,15 +38,8 @@
 // -------------------------------------------------------------------------------
 - (void)awakeFromNib {
     [verticalSplitView setDelegate:self];    // we want a chance to affect the vertical split view coverage
-    [self.genotypeTableView setTarget:self];
-    [self.genotypeTableView setAllowsColumnSelection:TRUE];
-    [self.genotypeTableView setSelectionHighlightStyle:NSTableViewSelectionHighlightStyleNone];
-    [self.genotypeTableView setAction:@selector(genotypeSelected:)];
     self.mainWindow.backgroundColor = [NSColor whiteColor];
-
     self.stacksLoader = [[StacksLoader alloc] init];
-
-
 }
 
 
@@ -57,16 +47,6 @@
 //	splitView:effectiveRect:effectiveRect:forDrawnRect:ofDividerAtIndex
 // -------------------------------------------------------------------------------
 - (NSRect)splitView:(NSSplitView *)splitView effectiveRect:(NSRect)proposedEffectiveRect forDrawnRect:(NSRect)drawnRect ofDividerAtIndex:(NSInteger)dividerIndex {
-//    NSRect effectiveRect = drawnRect;
-//
-//    if (splitView == verticalSplitView) {
-//        // don't steal as much from the scroll bar as NSSplitView normally would
-//        effectiveRect.origin.x -= 2.0;
-//        effectiveRect.size.width += 6.0;
-//
-//    }
-//
-//    return effectiveRect;
     return NSZeroRect;
 }
 
@@ -100,20 +80,7 @@
 }
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
-    if ([[tableView identifier] isEqualToString:@"GenotypeTableView"]) {
-        if (self.selectedLocusView != nil) {
-            LocusView *locusView = self.selectedLocusView;
-//            NSInteger count = [locusView genotypes];
-            NSInteger count = locusView.genotypes.count;
-            NSInteger rows = count / 10;
-            if (count % 10 > 0) {
-                rows++;
-            }
-            return rows;
-        }
-        return 0;
-    }
-    else if ([[tableView identifier] isEqualToString:@"LocusTable"]) {
+    if ([[tableView identifier] isEqualToString:@"LocusTable"]) {
         return [self.stacksDocument.locusViews count];
     }
     else if ([[tableView identifier] isEqualToString:@"StacksTableView"]) {
@@ -129,14 +96,10 @@
 
 - (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
 
-
     // Get a new ViewCell
     NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
 
-    if ([[tableView identifier] isEqualToString:@"GenotypeTableView"]) {
-        return [self handleGenotypesTable:(NSString *) tableColumn.identifier row:(NSInteger) row cell:(NSTableCellView *) cellView];
-    }
-    else if ([[tableView identifier] isEqualToString:@"LocusTable"]) {
+    if ([[tableView identifier] isEqualToString:@"LocusTable"]) {
         // we want data for the row . . . .
         NSArray *sortedKeys = [[self.stacksDocument.locusViews allKeys] sortedArrayUsingComparator:(NSComparator) ^(id obj1, id obj2) {
             return [obj1 integerValue] - [obj2 integerValue];
@@ -242,7 +205,6 @@
                 }
                     break;
                 default: {
-
                     NSString *sequenceString = [(StackEntry *) [stacksView.stackEntries objectAtIndex:row - 3] sequence];
                     cellView.textField.attributedStringValue = [self createSnpsView:sequenceString snps:stacksView.snps];
                     cellView.textField.font = [NSFont fontWithName:@"Courier" size:14];
@@ -260,9 +222,7 @@
 
 - (NSAttributedString *)createReferenceView:(NSUInteger)sequenceSize {
     // create a string from 0-9 for sequenceSize
-
     NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:@"123123123"];
-
 
     return string;
 }
@@ -285,57 +245,6 @@
     return string;
 }
 
-- (NSTableCellView *)handleGenotypesTable:(NSString *)column row:(NSInteger)row cell:(GenotypeCell *)cellView {
-    if (self.stacksDocument != nil) {
-
-        LocusView *locusView = self.selectedLocusView;
-        NSInteger progenyCount = locusView.genotypes.count;
-
-        // TODO: this should come from the file system, nowhere else
-// to identify the # of parents: look at "identify_parents" in genotypes.cc .
-        NSInteger totalColumnCount = 10;
-//        NSInteger remainderColumns = totalCount % totalColumnCount;
-
-
-        // turn the row / column into an index
-        NSInteger columnIndex = [[column substringFromIndex:9] integerValue];
-        NSInteger progenyIndex = row * totalColumnCount + columnIndex;
-
-        if (progenyCount > progenyIndex) {
-            NSArray *sortedKeys = [[locusView.genotypes allKeys] sortedArrayUsingComparator:^(NSString *obj1, NSString *obj2) {
-                return [obj1 compare:obj2];
-            }];
-            NSString *key = [sortedKeys objectAtIndex:progenyIndex - 1];
-            GenotypeEntry *genotypeEntry = [locusView.genotypes valueForKey:key];
-//            cellView.textField.stringValue = [NSString stringWithFormat:@"%@  %@", genotypeEntry.name, [genotypeEntry render]];
-
-            NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:genotypeEntry.name];
-
-
-            NSMutableAttributedString *haplotypes = [[NSMutableAttributedString alloc] initWithString:@""];
-
-            [haplotypes beginEditing];
-
-            for (NSString *haplotype in genotypeEntry.haplotypes) {
-                NSMutableAttributedString *hapString = [[NSMutableAttributedString alloc] initWithString:haplotype];
-                [haplotypes appendAttributedString:hapString];
-            }
-
-            [haplotypes endEditing];
-
-            cellView.textField.attributedStringValue = string;
-//            cellView.haplotypes.attributedStringValue = haplotypes;
-        }
-        else {
-            cellView.textField.stringValue = @"";
-        }
-    }
-    else {
-        cellView.textField.stringValue = @"";
-    }
-
-    return cellView;
-}
 
 - (LocusView *)findSelectedLocus {
     NSInteger selectedRow = [self.locusTableView selectedRow];
@@ -352,13 +261,11 @@
     if (locus != nil) {
         NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:locus.consensus];
 
-
         NSRange selectedRange = NSMakeRange(12, 1);
 
         [string beginEditing];
 
         [NSFont boldSystemFontOfSize:10.0];
-
         NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:
                 [NSColor blueColor], NSForegroundColorAttributeName,
                 [NSColor grayColor], NSBackgroundColorAttributeName,
@@ -369,30 +276,11 @@
         [string endEditing];
 
 
-        NSMutableArray *genotypes = [[NSMutableArray alloc] init];
-        [genotypes addObjectsFromArray:[locus.genotypes allValues]];
-
-        NSLog(@"input values: %ld vs %ld", locus.genotypes.count, genotypes.count);
-
-        self.selectedGenotypes = genotypes;
-
-        NSLog(@"input values: %ld vs %ld", locus.genotypes.count, self.selectedGenotypes.count);
-
-//        [self.selectedGenotypes setValue:genotypes];
-
-        NSLog(@"should be setting genotypes %ld", self.selectedGenotypes.count);
-
-//        [self.genotypeTableView reloadData];
-
+        self.selectedGenotypes = [locus.genotypes allValues];
     }
     else {
         self.stacksDocument = nil ;
-//        [self.locusDetail setStringValue:@"Error"];
     }
-
-
-    // update the genotypes table
-//    [self.genotypeTableView reloadData];
 
 
 }
@@ -413,39 +301,6 @@
     [self.stacksTableView reloadData];
 }
 
-
-// TODO: handle genotype selection
-- (void)genotypeSelected:(id)tableView {
-    NSInteger rowNumber = [_genotypeTableView clickedRow];
-    NSInteger columnNumber = [_genotypeTableView clickedColumn];
-    if (rowNumber < 0 || columnNumber < 0) {
-        NSLog(@"invalid selection");
-        self.selectedStacks = nil ;
-        return;
-    }
-    // get the array number
-
-    LocusView *locusView = self.selectedLocusView;
-    NSUInteger totalColumnCount = 10;
-
-    int index = rowNumber * totalColumnCount + columnNumber;
-    NSLog(@"loading genotypes %d", locusView.genotypes.count);
-
-    if (index + 1 < locusView.genotypes.count) {
-        NSString *key = [[locusView.genotypes allKeys] objectAtIndex:index + 1];
-        GenotypeEntry *genotypeEntry = [locusView.genotypes valueForKey:key];
-        NSLog(@"entry ID: %d", genotypeEntry.sampleId);
-
-        NSLog(@"loading %@ tag - %d", genotypeEntry.name, genotypeEntry.tagId);
-        StacksView *stacksView = [_stacksLoader loadStacksView:genotypeEntry.name atPath:@"/tmp/stacks_tut/" forTag:genotypeEntry.tagId locus:locusView];
-        self.selectedStacks = stacksView;
-    }
-    else {
-        NSLog(@"invalid selection");
-        self.selectedStacks = nil ;
-    }
-    [self showTagsTable:self.selectedStacks];
-}
 
 @end
 
