60ce0e0d7e708abe4e31ca6d067b81014c8dca81
Nathan Dunn <ndunn@me.com>
2013-04-17 11:12:07 -0700

    sped up load time significantly

--- MacOSXClient/StacksGUI2/GenotypeEntry.h
+++ MacOSXClient/StacksGUI2/GenotypeEntry.h
@@ -30,6 +30,9 @@
 @property NSInteger  tagId;
 
 - (NSString *)render;
+
+- (id)initWithCapcity:(int)numHaplotypes;
+
 - (NSAttributedString *)renderName;
 - (NSAttributedString *)renderHaplotypes;
 - (NSAttributedString *)renderDepths;
