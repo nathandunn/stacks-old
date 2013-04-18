ea5a300c770c3fc7e04d8f922dbe6c831592c796
Nathan Dunn <ndunn@uoregon.edu>
2013-04-18 11:27:15 -0700

    added next phase

---
+++ MacOSXClient/StacksGui3/StacksGui3Tests/StacksGui3Tests.h
@@ -0,0 +1,13 @@
+//
+//  StacksGui3Tests.h
+//  StacksGui3Tests
+//
+//  Created by Nathan Dunn on 4/18/13.
+//  Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StacksGui3Tests : SenTestCase
+
+@end
