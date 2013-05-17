0689820a14ac57e1125c87a315120c175d9b643d
Nathan Dunn <ndunn@uoregon.edu>
2013-05-17 11:00:18 -0700

    got good soring and fixed population bug

---
+++ MacOSXClient/StacksGui3/StacksGui3/LocusArrayController.m
@@ -0,0 +1,21 @@
+//
+//  LocusArrayController.m
+//  StacksGui3
+//
+//  Created by Nathan Dunn on 5/17/13.
+//  Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+
+#import "LocusArrayController.h"
+
+@implementation LocusArrayController
+
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self setSortDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"locusId" ascending:YES selector:@selector(compare:)]]];
+
+}
+
+
+@end
