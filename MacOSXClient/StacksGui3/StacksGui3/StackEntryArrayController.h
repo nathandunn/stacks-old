ccce4ccc3a5670e7b2d7205a56f29aa9a8f68344
Nathan Dunn <ndunn@uoregon.edu>
2013-05-17 11:04:35 -0700

    added the array controller

---
+++ MacOSXClient/StacksGui3/StacksGui3/StackEntryArrayController.h
@@ -0,0 +1,13 @@
+//
+//  StackEntryArrayController.h
+//  StacksGui3
+//
+//  Created by Nathan Dunn on 5/17/13.
+//  Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StackEntryArrayController : NSArrayController
+
+@end
