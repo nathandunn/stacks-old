18b846cd04b659562519dbba1aba6e8b6a862def
Nathan Dunn <ndunn@me.com>
2013-05-26 16:32:02 -0700

    added to collection view

---
+++ MacOSXClient/StacksGui3/StacksGui3/DatumCollectionViewItem.h
@@ -0,0 +1,14 @@
+//
+// Created by Nathan Dunn on 5/26/13.
+// Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@class DatumView;
+
+@interface DatumCollectionViewItem : NSCollectionViewItem
+@end
