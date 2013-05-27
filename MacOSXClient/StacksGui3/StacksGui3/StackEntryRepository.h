3352e0b7bb18f877eeb08d1e7b6392a761b15597
Nathan Dunn <ndunn@me.com>
2013-05-26 21:33:21 -0700

    updated

--- MacOSXClient/StacksGui3/StacksGui3/StackEntryRepository.h
+++ MacOSXClient/StacksGui3/StacksGui3/StackEntryRepository.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 @class StackEntryMO;
-@class StackMO;
 @class DatumMO;
 @class ConsensusStackEntryMO;
 @class ModelStackEntryMO;
+@class ReferenceStackEntryMO;
 
 @interface StackEntryRepository : NSObject
 - (StackEntryMO *)insertStackEntry:(NSManagedObjectContext *)context entryId:(NSNumber *)id relationship:(NSString *)relationship block:(NSString *)block sequenceId:(NSString *)sequenceId sequence:(NSString *)sequence datum:(DatumMO *)datum;
@@ -20,4 +20,6 @@
 - (ConsensusStackEntryMO *)insertConsensusStackEntry:(NSManagedObjectContext *)context entryId:(NSNumber *)id block:(id)block sequenceId:(id)id1 sequence:(id)sequence datum:(DatumMO *)datum;
 
 - (ModelStackEntryMO *)insertModelStackEntry:(NSManagedObjectContext *)context entryId:(NSNumber *)entryId block:(NSString *)block sequenceId:(NSString *)sequenceId sequence:(id)sequence datum:(DatumMO *)datum;
+
+- (ReferenceStackEntryMO *)insertReferenceStackEntry:(NSManagedObjectContext *)context sequence:(NSString *)sequence datum:(DatumMO *)datum;
 @end
