22d50cb0e57dc7e5915d7a82371c2da3020671df
Nathan Dunn <ndunn@me.com>
2013-08-30 14:51:21 -0700

    added new controller for the file item menus

---
+++ MacOSXClient/StacksGui3/StacksGui3/StacksDocumentController.m
@@ -0,0 +1,18 @@
+//
+//  StacksDocumentController.m
+//  StacksGui3
+//
+//  Created by Nathan Dunn on 8/30/13.
+//  Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+
+#import "StacksDocumentController.h"
+
+@implementation StacksDocumentController
+
+//- (BOOL)validateUserInterfaceItem:(id <NSValidatedUserInterfaceItem>)anItem {
+//    return [super validateUserInterfaceItem:anItem];
+//}
+
+
+@end
