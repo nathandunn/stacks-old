559d48a8018d84a980ec6e950c6085329f3f8ef8
Nathan Dunn <ndunn@uoregon.edu>
2013-09-09 13:47:39 -0700

    updated presentation

---
+++ MacOSXClient/StacksGui3/StacksGui3/StacksApplicationController.h
@@ -0,0 +1,16 @@
+//
+// Created by Nathan Dunn on 9/9/13.
+// Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface StacksApplicationController : NSDocumentController
+
+
+
+@end
---
+++ MacOSXClient/StacksGui3/StacksGui3/StacksApplicationController.m
@@ -0,0 +1,31 @@
+//
+// Created by Nathan Dunn on 9/9/13.
+// Copyright (c) 2013 Nathan Dunn. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "StacksApplicationController.h"
+
+
+@implementation StacksApplicationController {
+
+
+
+}
+//- (id)init {
+//    self = [super init];
+//    if (self) {
+//
+//    }
+//
+//    return self;
+//}
+
+//- (BOOL)validateUserInterfaceItem:(id <NSValidatedUserInterfaceItem>)anItem {
+//    return [super validateUserInterfaceItem:anItem];
+//}
+
+
+@end
