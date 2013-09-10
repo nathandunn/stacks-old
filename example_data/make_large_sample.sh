1108ab17f168c5514ca278aca12382f37d5e0d6f
Nathan Dunn <ndunn@uoregon.edu>
2013-09-10 12:14:17 -0700

    added additional data with updates

--- example_data/make_large_sample.sh
+++ example_data/make_large_sample.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #gunzip pop_stacks_example.tar.gz
 TARGET=$HOME/Desktop/stacks_large
+rm -rf nathan_stacks
 tar xf pop_stacks_example.tar
 mv -f $TARGET $TARGET.old
 #rm -rf $HOME/Desktop/stacks_large
