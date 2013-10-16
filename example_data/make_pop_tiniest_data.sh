efceb78a7110e4e74992f6a9253c06c84653016a
Nathan Dunn <ndunn@me.com>
2013-10-16 16:37:19 -0700

    updated client

--- example_data/make_pop_tiniest_data.sh
+++ example_data/make_pop_tiniest_data.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-TARGET=$HOME/Desktop/pop_stacks1
-rm -rf pop_stacks_tiny
-tar xfz pop_stacks_tiny.tgz
+TARGET=$HOME/Desktop/pop_stacks_tiniest
+rm -rf pop_stacks_tiniest
+tar xfz pop_stacks_tiniest.tgz
 rm -rf $TARGET
-mv pop_stacks_tiny $TARGET
+mv pop_stacks_tiniest $TARGET
 #cp popmap_stacks_tut.txt $TARGET/popmap
 #rm -f $TARGET/popmap
 
