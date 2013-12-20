353870b3159fecc729decf0ded4192d20a783e6a
Nathan Dunn <ndunn@uoregon.edu>
2013-12-20 13:36:18 -0800

    using mutable string helps make faster and more efficient

--- MacOSXClient/StacksGui3/StacksGui3/StacksEntryView.m
+++ MacOSXClient/StacksGui3/StacksGui3/StacksEntryView.m
@@ -13,12 +13,14 @@
 
 @synthesize sampleName ;
 @synthesize locusId;
+@synthesize sequenceIds;
 @synthesize sequences;
 
 - (id)init {
     self = [super init];
     if (self) {
         sequences = [[NSMutableArray alloc] init];
+        sequenceIds = [[NSMutableArray alloc] init];
     }
 
     return self;
@@ -28,18 +30,26 @@
 - (NSString *)renderHtml {
      NSString* returnHTML = [NSString stringWithFormat:@"<table><tr><td col=4>RENDERED Some stack data for sample '%@' and locus '%ld' and # stacks: %ld</td></tr>",sampleName,locusId,[sequences count]];
 
-    returnHTML = [NSString stringWithFormat:@"%@%@",returnHTML,[self renderSequences]]; ;
+//    returnHTML = [NSString stringWithFormat:@"%@%@",returnHTML,[self renderSequences]]; ;
 
 
+    NSString* sequenceString = [self renderSequences];
 
-    returnHTML = [NSString stringWithFormat:@"%@</table>",returnHTML];
-    return returnHTML ;
+    return [NSString stringWithFormat:@"%@%@</table>",returnHTML,sequenceString];
 }
 
 - (NSString *)renderSequences {
-    NSString* returnString = @"";
-    for(NSString* sequence in sequences){
-        returnString = [NSString stringWithFormat:@"%@<tr><td col=4>%@</td></tr>",returnString,sequence];
+    NSMutableString* returnString = [NSMutableString string];
+
+//    NSString *sequence ;
+//    NSString *sequenceId ;
+    for(int i = 0 ; i < sequences.count  ; i++){
+//        sequence = [sequences objectAtIndex:i];
+//        sequenceId = [sequenceIds  objectAtIndex:i];
+//        returnString = [NSString stringWithFormat:@"%@<tr><td>%@</td><td col=3>%@</td></tr>",returnString,[sequenceIds objectAtIndex:i]
+//                ,[sequences objectAtIndex:i]];
+        [returnString appendFormat:@"<tr><td>%@</td><td col=3>%@</td></tr>",[sequenceIds objectAtIndex:i]
+                ,[sequences objectAtIndex:i]];
     }
     return returnString;
 }
