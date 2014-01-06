5151e54b74f875d04ace412aadc4ca2a89aed18c
Nathan Dunn <ndunn@me.com>
2014-01-06 14:26:22 -0800

    using the Julian css

--- vStacks/StacksGui3/StacksEntryView.m
+++ vStacks/StacksGui3/StacksEntryView.m
@@ -18,6 +18,8 @@
 @synthesize blocks ;
 @synthesize relationships;
 @synthesize entryIds;
+@synthesize model;
+@synthesize consensus;
 
 - (id)init {
     self = [super init];
@@ -35,7 +37,8 @@
 
 - (NSString *)renderHtml {
 //    NSString* headerHTML= @"<head><link rel='stylesheet' type='text/css' href='test.css'/></head><body>" ;
-    NSString* cssPath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"css"];
+//    NSString* cssPath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"css"];
+    NSString* cssPath = [[NSBundle mainBundle] pathForResource:@"stacks" ofType:@"css"];
 //    NSLog(@"css path %@",cssPath) ;
     NSString *cssString = [NSString stringWithContentsOfFile:cssPath encoding:NSUTF8StringEncoding error:NULL];
 //    NSLog(@"css string %@",cssString) ;
@@ -43,7 +46,7 @@
 //    NSString* returnHTML = [NSString stringWithFormat:@"<style type='text/css'>%@</style><div class='sample'>testy</div><table><tr><td col=4><div class='sample'>RENDERED Some stack data for sample '%@' and locus '%ld' and # stacks: %ld</div></td></tr>",cssString,sampleName,locusId,[sequences count]];
     
 //    NSMutableString* returnHTML = [NSMutableString stringWithFormat:@"<style type='text/css'>%@</style><div class='sample'>testy</div><table><tr><td col=4><div class='sample'>RENDERED Some stack data for sample '%@' and locus '%ld' and # stacks: %ld</div></td></tr>",cssString,sampleName,locusId,[sequences count]];
-    NSMutableString* returnHTML = [NSMutableString stringWithFormat:@"<style type='text/css'>%@</style><table>",cssString];
+    NSMutableString* returnHTML = [NSMutableString stringWithFormat:@"<style type='text/css'>%@</style><table class='radtag'>",cssString];
     [returnHTML appendString:[self renderReference]];
     [returnHTML appendString:[self renderConsensus]];
     [returnHTML appendString:[self renderModel]];
@@ -54,24 +57,37 @@
 
 
 - (NSString *)renderReference {
-    return @"";
+    NSMutableString* returnString = [[NSMutableString alloc] init];
+    NSMutableString *referenceString = [[NSMutableString alloc] init];
+    NSUInteger sequenceSize = consensus.length ;
+
+    for (NSUInteger i = 0; i < sequenceSize; i++) {
+        [referenceString appendFormat:@"%ld", i % 10];
+    }
+    [returnString appendFormat:@"<tr><td class='id'></td><td></td><td></td><td class='tag'>%@</td></tr>",referenceString];
+    return returnString ;
 }
 
 - (NSString *)renderConsensus {
-    return @"";
+    NSMutableString* returnString = [[NSMutableString alloc] init];
+    [returnString appendFormat:@"<tr><td class='id'></td><td class='con'>consensus</td><td></td><td class='tag'>%@</td></tr>",consensus];
+    return returnString ;
 }
 
 - (NSString *)renderModel {
-    return @"";
+    NSMutableString* returnString = [[NSMutableString alloc] init];
+    [returnString appendFormat:@"<tr><td class='id'></td><td class='num'>model</td><td></td><td class='tag'>%@</td></tr>",model];
+    return returnString ;
 }
 
 
 - (NSMutableString *)renderSequences {
+//    NSLog(@"sequences %ld entryIds %ld relatinships %ld sequenceIds %ld",sequences.count,entryIds.count,relationships.count,sequenceIds.count) ;
     NSMutableString* returnString = [[NSMutableString alloc] init];
 //    NSString *sequence  ;
 //    NSString *sequenceId  ;
     for(int i = 0 ; i < sequences.count ; i++){
-        [returnString appendFormat:@"<tr><td>%@</td><td>%@</td><td>%@</td><td>%@</td></tr>",[entryIds objectAtIndex:i],[relationships objectAtIndex:i],[sequenceIds objectAtIndex:i],[sequences objectAtIndex:i]];
+        [returnString appendFormat:@"<tr><td class='id'>%@</td><td class='%@'>%@</td><td>%@</td><td class='tag'>%@</td></tr>",[entryIds objectAtIndex:i],[relationships objectAtIndex:i],[relationships objectAtIndex:i],[sequenceIds objectAtIndex:i],[sequences objectAtIndex:i]];
     }
     return returnString;
 }
