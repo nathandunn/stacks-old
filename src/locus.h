64daf2b12511abc597eb5da41b31fab22c58037a
Julian Catchen <julian.catchen@gmail.com>
2014-02-10 00:27:52 -0800

    Started implementation of kernel smoothing for amova phi_st.

--- src/locus.h
+++ src/locus.h
@@ -119,6 +119,7 @@ public:
 	this->hcnt = 0; 
 	this->gcnt = 0; 
 	this->trans_gcnt = 0; 
+	this->hap_fstats = NULL;
     };
     string annotation;
     string marker;
@@ -128,6 +129,7 @@ public:
     int gcnt;                 // Number of progeny containing a valid genotype.
     int trans_gcnt;           // Number of progeny containing a valid 
                               // genotype, translated for a particular map type.
+    double *hap_fstats;
 };
 
 bool bp_compare(Locus *, Locus *);
