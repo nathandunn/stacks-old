2e74179f0cba19a708c32e72c44fbff18069644e
Nathan Dunn <ndunn@me.com>
2014-02-20 15:13:58 -0800

    fixed license headers

--- vStacks/vStacks/DNANSeq.h
+++ vStacks/vStacks/DNANSeq.h
@@ -1,21 +1,7 @@
 // -*-mode:c++; c-style:k&r; c-basic-offset:4;-*-
 //
-// Copyright 2011, Julian Catchen <jcatchen@uoregon.edu>
-//
-// This file is part of Stacks.
-//
-// Stacks is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Stacks is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Stacks.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (c) 2014 University of Oregon
+// Created by Julian Catchen <jcatchen@uoregon.edu>
 //
 
 #ifndef __DNANSeq_H__
