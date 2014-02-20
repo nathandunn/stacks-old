7f8150a97a9b715b8e47338c09c1b327652d9d8b
Nathan Dunn <ndunn@me.com>
2014-02-20 14:52:20 -0800

    removed unnecessary files

--- example_data/make_new_tiny_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-TARGET=$HOME/Desktop/new_tut_tiny
-rm -rf stacks_tiny
-tar xfz stacks_new_tut_tiny.tgz
-rm -rf $TARGET
-mv stacks_tiny $TARGET
-#cp popmap_stacks_tut.txt $TARGET/popmap
-#rm -f $TARGET/popmap
-
