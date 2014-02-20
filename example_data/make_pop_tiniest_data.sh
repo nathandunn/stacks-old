80d24957b547dfc24bb9d2b2164280cc28118460
Nathan Dunn <ndunn@me.com>
2014-02-20 14:52:20 -0800

    removed unnecessary files

--- example_data/make_pop_tiniest_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-TARGET=$HOME/Desktop/pop_stacks_tiniest
-rm -rf pop_stacks_tiniest
-tar xfz pop_stacks_tiniest.tgz
-rm -rf $TARGET
-mv pop_stacks_tiniest $TARGET
-#cp popmap_stacks_tut.txt $TARGET/popmap
-#rm -f $TARGET/popmap
-
--- example_data/make_tiny_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-TARGET=$HOME/Desktop/stacks_female_only
-rm -rf stacks_tut
-tar xfz female_only_stacks_tut.tgz
-rm -rf $TARGET
-mv stacks_tut $TARGET
-#cp popmap_stacks_tut.txt $TARGET/popmap
-rm -f $TARGET/popmap
-
