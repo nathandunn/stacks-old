2e09b5a4b631cfacdb324393c816f2e899847bcc
Nathan Dunn <ndunn@me.com>
2014-03-26 12:30:37 -0700

    added GenericHash MO

---
+++ vStacks/vStacks/GenericHashMO.m
@@ -0,0 +1,19 @@
+//
+//  GenericHash.m
+//  vStacks
+//
+//  Created by Nathan Dunn on 3/26/14.
+//  Copyright (c) 2014 Nathan Dunn. All rights reserved.
+//
+
+#import "GenericHashMO.h"
+
+
+@implementation GenericHashMO
+
+@dynamic dataValue;
+@dynamic key;
+@dynamic stringValue;
+@dynamic type;
+
+@end
