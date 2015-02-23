c8e95ba7da8be6470baf6b1ec4095e5352b00475
Julian Catchen <julian.catchen@gmail.com>
2015-02-23 11:17:21 -0800

    Added check to wrappers to stop processing if catalog creation fails.

--- scripts/ref_map.pl
+++ scripts/ref_map.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# Copyright 2010-2014, Julian Catchen <jcatchen@uoregon.edu>
+# Copyright 2010-2015, Julian Catchen <jcatchen@illinois.edu>
 #
 # This file is part of Stacks.
 #
@@ -25,8 +25,6 @@
 #
 # For the database interactions to work, the 'mysql' program is expected to be
 # on the path and sufficient permissions set to access the specified database.
-#
-# By Julian Catchen <jcatchen@uoregon.edu>
 #
 
 use strict;
