5c7d1d98080685df3cb76ba03489c6b9cae6cedd
Julian Catchen <julian.catchen@gmail.com>
2015-04-29 12:49:26 -0700

    About to merge new web interface code

--- php/stacks_functions.php
+++ php/stacks_functions.php
@@ -23,12 +23,9 @@ function write_header($page_title) {
     global $root_path, $img_path;
 
     echo <<< EOQ
-<?xml version="1.0" encoding="iso-8859-1"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
+<html>
 <head>
+  <meta charset="utf-8">
   <title>$site_title: $page_title</title>
   <link rel="stylesheet" type="text/css" href="$root_path/stacks.css" />
   <script type="text/javascript" src="$root_path/stacks.js"></script>
