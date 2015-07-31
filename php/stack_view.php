70ed34bad6ecc3c78edd1cbca3f73ac5b205ffc3
Julian Catchen <julian.catchen@gmail.com>
2015-07-31 13:06:29 -0700

    Fixed indentation on PHP files

--- php/stack_view.php
+++ php/stack_view.php
@@ -58,131 +58,132 @@ $db['cat_snp_sth'] = $db['dbh']->prepare($query);
 check_db_error($db['cat_snp_sth'], __FILE__, __LINE__);
 
 $json_str = 
-  "{" .
-  "\"id\": \"$cat_id\"," .
-  "\"stacks\": [";
+    "{" .
+    "\"id\": \"$cat_id\"," .
+    "\"stacks\": [";
 
 for ($i = 0; $i < count($tags); $i++) {
-  $sample_id = $samples[$i];
-  $tag_id    = $tags[$i];
+    $sample_id = $samples[$i];
+    $tag_id    = $tags[$i];
 
-  //
-  // Fetch and store the SNPs.
-  //
-  $snps   = array();
-  $result = $db['snp_sth']->execute(array($batch_id, $sample_id, $tag_id));
-  check_db_error($result, __FILE__, __LINE__);
-  while ($row = $result->fetchRow()) {
-      $snps[$row['col']] = array('col' => $row['col'], 'rank_1' => $row['rank_1'], 'rank_2' => $row['rank_2']);
-  }
+    //
+    // Fetch and store the SNPs.
+    //
+    $snps   = array();
+    $result = $db['snp_sth']->execute(array($batch_id, $sample_id, $tag_id));
+    check_db_error($result, __FILE__, __LINE__);
+    while ($row = $result->fetchRow()) {
+        $snps[$row['col']] = array('col' => $row['col'], 'rank_1' => $row['rank_1'], 'rank_2' => $row['rank_2']);
+    }
 
-  //
-  // Fetch and store the catalog SNPs.
-  //
-  $cat_snps = array();
-  $result   = $db['cat_snp_sth']->execute(array($batch_id, $cat_id));
-  check_db_error($result, __FILE__, __LINE__);
-  while ($row = $result->fetchRow()) {
-    if (!isset($cat_snps[$row['col']]))
-      $cat_snps[$row['col']] = 0;
-    $cat_snps[$row['col']]++;
-  }
+    //
+    // Fetch and store the catalog SNPs.
+    //
+    $cat_snps = array();
+    $result   = $db['cat_snp_sth']->execute(array($batch_id, $cat_id));
+    check_db_error($result, __FILE__, __LINE__);
+    while ($row = $result->fetchRow()) {
+        if (!isset($cat_snps[$row['col']]))
+            $cat_snps[$row['col']] = 0;
+        $cat_snps[$row['col']]++;
+    }
 
-  //
-  // Fetch the sequences.
-  //
-  $deleveraged     = 0;
-  $lumberjackstack = 0;
-  $blacklisted     = 0;
-  $consensus       = "";
-  $model           = "";
-  $stacks          = array();
-  $secondary       = array();
-  $result = $db['seq_sth']->execute(array($batch_id, $sample_id, $tag_id));
-  check_db_error($result, __FILE__, __LINE__);
+    //
+    // Fetch the sequences.
+    //
+    $deleveraged     = 0;
+    $lumberjackstack = 0;
+    $blacklisted     = 0;
+    $consensus       = "";
+    $model           = "";
+    $file            = "";
+    $stacks          = array();
+    $secondary       = array();
+    $result = $db['seq_sth']->execute(array($batch_id, $sample_id, $tag_id));
+    check_db_error($result, __FILE__, __LINE__);
 
-  while ($row = $result->fetchRow()) {
+    while ($row = $result->fetchRow()) {
     
-    if ($row['relationship'] == "consensus") {
-      $deleveraged     = $row['deleveraged'];
-      $lumberjackstack = $row['removed'];
-      $blacklisted     = $row['blacklisted'];
-      $consensus       = $row['seq'];
-      $file            = $row['file'];
+        if ($row['relationship'] == "consensus") {
+            $deleveraged     = $row['deleveraged'];
+            $lumberjackstack = $row['removed'];
+            $blacklisted     = $row['blacklisted'];
+            $consensus       = $row['seq'];
+            $file            = $row['file'];
 
-    } else if ($row['relationship'] == "model") {
-      $model = $row['seq'];
+        } else if ($row['relationship'] == "model") {
+            $model = $row['seq'];
 
-    } else if ($row['relationship'] == "secondary") {
-      array_push($secondary, array('s' => $row['seq'], 'id' => $row['seq_id']));
+        } else if ($row['relationship'] == "secondary") {
+            array_push($secondary, array('s' => $row['seq'], 'id' => $row['seq_id']));
 
-    } else {
-      if (!isset($stacks[$row['sub_id']]))
-	$stacks[$row['sub_id']] = array();
-      array_push($stacks[$row['sub_id']], array('s' => $row['seq'], 'id' => $row['seq_id']));
-    }
-  }
-
-  $con_len = strlen($consensus);
-
-  $c = print_snps($tags[$i], $consensus, $consensus, $snps, false);
-  $c = addslashes($c);
-
-  $scale = print_scale($con_len);
-  $scale = addslashes($scale);
-
-  $json_str .= 
-    "{" .
-    "\"sample_id\": \"$sample_id\"," .
-    "\"sample_name\": \"$file\"," .
-    "\"tag_id\": \"$tag_id\"," .
-    "\"consensus\": \"$c\"," .
-    "\"model\": \"$model\"," .
-    "\"scale\": \"$scale\"," .
-    "\"primary\": [";
-
-  foreach ($stacks as $sub_id => $stack) {
-    $s = print_snps_errs($consensus, $stack[0]['s'], $snps, $cat_snps);
-    $s = addslashes($s);
-
-    $json_str .=
-      "{" .
-      "\"seq\": \"$s\"," .
-      "\"ids\": [";
-
-    foreach ($stack as $seq) {
-      $json_str .=
-	"\"$seq[id]\",";
+        } else {
+            if (!isset($stacks[$row['sub_id']]))
+                $stacks[$row['sub_id']] = array();
+            array_push($stacks[$row['sub_id']], array('s' => $row['seq'], 'id' => $row['seq_id']));
+        }
     }
 
-    if (count($stack) > 0)
-      $json_str = substr($json_str, 0, -1);
+    $con_len = strlen($consensus);
+
+    $c = print_snps($tags[$i], $consensus, $consensus, $snps, false);
+    $c = addslashes($c);
+
+    $scale = print_scale($con_len);
+    $scale = addslashes($scale);
+
     $json_str .= 
-      "]" .
-      "},";
-  }
+        "{" .
+        "\"sample_id\": \"$sample_id\"," .
+        "\"sample_name\": \"$file\"," .
+        "\"tag_id\": \"$tag_id\"," .
+        "\"consensus\": \"$c\"," .
+        "\"model\": \"$model\"," .
+        "\"scale\": \"$scale\"," .
+        "\"primary\": [";
 
-  if (count($stacks) > 0)
-    $json_str = substr($json_str, 0, -1);
-  $json_str .=
-    "]," .
-    "\"secondary\": [";
+    foreach ($stacks as $sub_id => $stack) {
+        $s = print_snps_errs($consensus, $stack[0]['s'], $snps, $cat_snps);
+        $s = addslashes($s);
 
-  foreach ($secondary as $seq) {
-    $s = print_snps_errs($consensus, $seq['s'], $snps, $cat_snps);
-    $s = addslashes($s);
+        $json_str .=
+            "{" .
+            "\"seq\": \"$s\"," .
+            "\"ids\": [";
 
+        foreach ($stack as $seq) {
+            $json_str .=
+                "\"$seq[id]\",";
+        }
+
+        if (count($stack) > 0)
+            $json_str = substr($json_str, 0, -1);
+        $json_str .= 
+            "]" .
+            "},";
+    }
+
+    if (count($stacks) > 0)
+        $json_str = substr($json_str, 0, -1);
     $json_str .=
-      "{" .
-      "\"id\": \"$seq[id]\"," .
-      "\"seq\": \"$s\"" .
-      "},";
-  }
+        "]," .
+        "\"secondary\": [";
 
-  if (count($secondary) > 0)
-    $json_str = substr($json_str, 0, -1);
-  $json_str .= 
-    "]},";
+    foreach ($secondary as $seq) {
+        $s = print_snps_errs($consensus, $seq['s'], $snps, $cat_snps);
+        $s = addslashes($s);
+
+        $json_str .=
+            "{" .
+            "\"id\": \"$seq[id]\"," .
+            "\"seq\": \"$s\"" .
+            "},";
+    }
+
+    if (count($secondary) > 0)
+        $json_str = substr($json_str, 0, -1);
+    $json_str .= 
+        "]},";
 }
 
 $json_str  = substr($json_str, 0, -1);
