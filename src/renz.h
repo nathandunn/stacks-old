b31ba90f967f356aee17654fb791b5f7dc21df05
Julian Catchen <julian.catchen@gmail.com>
2016-01-29 13:41:29 -0800

    Added AseI restriction enzyme

--- src/renz.h
+++ src/renz.h
@@ -36,6 +36,8 @@ const char *apeKI[]   = {"CAGC", "CTGC",      // G/CWGC, ApeKI; W=A or T
  			 "GTCG", "GACG"};
 const char *apoI[]    = {"AATTC", "AATTT",    // R/AATTY, ApoI  (also known as XapI)
                          "GAATT", "AAATT"};
+const char *aseI[]    = {"TAAT",              // AT/TAAT, AseI
+                         "ATTA"};
 const char *bamHI[]   = {"GATCC",             // G/GATCC, BamHI
 			 "GGATC"};
 const char *bfaI[]    = {"TAG",               // C/TAG, BfaI
@@ -144,6 +146,7 @@ initialize_renz(map<string, const char **> &renz, map<string, int> &renz_cnt, ma
     renz["rsaI"]    = rsaI;    // GT/AC, RsaI
     renz["aciI"]    = aciI;    // C/CGC, AciI
     renz["bfaI"]    = bfaI;    // C/TAG, BfaI
+    renz["aseI"]    = aseI;    // AT/TAAT, AseI
 
     renz_cnt["sbfI"]    = 1;
     renz_cnt["pstI"]    = 1;
@@ -183,7 +186,8 @@ initialize_renz(map<string, const char **> &renz, map<string, int> &renz_cnt, ma
     renz_cnt["rsaI"]    = 1;
     renz_cnt["aciI"]    = 2;
     renz_cnt["bfaI"]    = 1;
-        
+    renz_cnt["aseI"]    = 1;
+
     renz_len["sbfI"]    = 6;
     renz_len["pstI"]    = 5;
     renz_len["notI"]    = 6;
@@ -222,7 +226,7 @@ initialize_renz(map<string, const char **> &renz, map<string, int> &renz_cnt, ma
     renz_len["rsaI"]    = 2;
     renz_len["aciI"]    = 3;
     renz_len["bfaI"]    = 3;
-
+    renz_len["aseI"]    = 4;
 }
 
 void 
