e8c782fa3f36e306584bb8860142eafbb846a4b3
Julian Catchen <julian.catchen@gmail.com>
2016-04-06 18:42:50 -0500

    Working on cstacks gapped alignments

--- src/cstacks.h
+++ src/cstacks.h
@@ -75,11 +75,12 @@ int  initialize_existing_catalog(string, map<int, CLocus *> &);
 int  update_catalog_index(map<int, CLocus *> &, map<string, int> &);
 int  find_kmer_matches_by_sequence(map<int, CLocus *> &, map<int, QLocus *> &, int);
 int  search_for_gaps(map<int, CLocus *> &, map<int, QLocus *> &, double, double);
+int  adjust_snps_for_gaps(vector<pair<char, uint> > &, Locus *);
 int  find_matches_by_sequence(map<int, CLocus *> &, map<int, QLocus *> &);
 int  find_matches_by_genomic_loc(map<string, int> &, map<int, QLocus *> &);
 int  characterize_mismatch_snps(CLocus *, QLocus *);
-int  merge_allele(Locus *locus, SNP *snp);
-int  merge_matches(map<int, CLocus *> &, map<int, QLocus *> &, pair<int, string> &, int, uint &);
+int  merge_allele(Locus *, SNP *);
+int  merge_matches(map<int, CLocus *> &, map<int, QLocus *> &, pair<int, string> &, int, uint &, uint &, uint &, uint &);
 int  add_unique_tag(pair<int, string> &, map<int, CLocus *> &, QLocus *);
 bool compare_dist(pair<int, int>, pair<int, int>);
 
