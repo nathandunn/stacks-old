23a66053811cd8d1127926349e1f3ef64973844b
Nicolas Rochette <rochette@illinois.edu>
2016-06-29 09:08:28 -0500

    Fixed an error introduced in merge [d07af729d69b].

--- src/populations.cc
+++ src/populations.cc
@@ -554,11 +554,8 @@ int main (int argc, char* argv[]) {
                              << "; likely IDs were mismatched when running pipeline.\n";
                         exit(0);
                     }
-                    d->len   = strlen(modres[d->id]->model);
-                    d->model = new char[d->len + 1];
-                    strcpy(d->model, modres[d->id]->model);
+                    d->add_model(modres[d->id]->model);
                 }
-                d->add_model(modres[d->id]->model);
             }
             for (mit = modres.begin(); mit != modres.end(); mit++)
                 delete mit->second;
