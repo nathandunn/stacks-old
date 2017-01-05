3b4844346e058caecfaa174216932519eb6b90de
Nicolas Rochette <rochette@illinois.edu>
2017-01-05 10:28:09 -0600

    pstacks: Changed the output.
    Also:
    * Changed when low-coverage loci are discarded;
    * moved the blacklisting test into the call_alleles() function and rewrote this function;
    * rewrote calc_coverage_distribution().
    
    Results are strictly identical for loc:female and gac_ak16:s13_an_01.

--- src/pstacks.h
+++ src/pstacks.h
@@ -78,14 +78,15 @@ void   version( void );
 int    parse_command_line(int, char**);
 void   load_radtags(string, HashMap &);
 int    reduce_radtags(HashMap &, map<int, PStack *> &);
-int    populate_merged_tags(map<int, PStack *> &, map<int, MergedStack *> &);
+void   populate_merged_tags(map<int, PStack *> &, map<int, MergedStack *> &);
+void   delete_low_cov_loci(map<int, MergedStack *>&, const map<int, PStack *>&);
 int    call_consensus(map<int, MergedStack *> &, map<int, PStack *> &, bool);
-int    call_alleles(MergedStack *, vector<DNANSeq *> &);
-int    count_raw_reads(map<int, PStack *> &, map<int, MergedStack *> &);
-double calc_coverage_distribution(map<int, PStack *> &, map<int, MergedStack *> &);
+void   call_alleles(MergedStack *, vector<DNANSeq *> &);
+void   calc_coverage_distribution(const map<int, PStack*>&, const map<int, MergedStack *>&, double&, double&, double&);
 int    write_results(map<int, MergedStack *> &, map<int, PStack *> &);
 
-//
+void report_options(std::ostream& os);
+
 // Debugging
 //
 int  dump_stacks(map<int, PStack *> &);
