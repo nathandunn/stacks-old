d6c3da8270100ba76730dc34c24a10eba0a3e091
Nicolas Rochette <rochette@illinois.edu>
2017-01-18 17:55:09 -0600

    tags2bam: Input should work.

--- src/tags2bam.h
+++ src/tags2bam.h
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
+
+#include "locus.h"
 
 void parse_command_line(int argc, char* argv[]);
 void report_options(std::ostream& os);
+
+void read_sample_files(std::map<int, Locus*>& sloci, std::unordered_map<int, int>& sloc_to_cloc);
+void write_bam_file(const std::map<int, Locus*>& sloci, const std::unordered_map<int, int>& sloc_to_cloc);
