3518756a391d874b1eec7ed54869ec8c5a944676
Nicolas Rochette <rochette@illinois.edu>
2017-01-18 17:55:09 -0600

    tags2bam: Input should work.

--- src/tags2bam.cc
+++ src/tags2bam.cc
@@ -4,6 +4,7 @@
 #include <getopt.h>
 
 #include "constants.h"
+#include "sql_utilities.h"
 #include "log_utils.h"
 
 #include "tags2bam.h"
@@ -37,10 +38,59 @@ int main(int argc, char* argv[]) {
     report_options(cout);
     cout << "\n";
 
-    // TODO
+    // Read the sample's files.
+    map<int, Locus*> sloci;
+    unordered_map<int, int> sloc_to_cloc;
+    read_sample_files(sloci, sloc_to_cloc);
+
+    // Write the BAM file.
+    write_bam_file(sloci, sloc_to_cloc);
 
     cout << "tags2bam is done." << endl;
     return 0;
+}
+
+void read_sample_files(map<int, Locus*>& sloci, unordered_map<int, int>& sloc_to_cloc) {
+    // Read the matches file.
+    cout << "Reading the matches file..." << endl;
+    vector<CatMatch*> matches;
+    load_catalog_matches(prefix_path, matches);
+    if (matches.empty()) {
+        cerr << "Error: Unable to load matches from '"
+             << prefix_path + ".matches.tsv(.gz)'.\n";
+        throw exception();
+    }
+    int sample_id = matches[0]->sample_id;
+
+    // Retrieve the list of bijective loci
+    vector<pair<int, int> > bij_loci = retrieve_bijective_loci(matches);
+    sloc_to_cloc.reserve(bij_loci.size());
+    sloc_to_cloc.insert(bij_loci.begin(), bij_loci.end());
+
+    // Read the tags file
+    cout << "Reading the tags file..." << endl;
+    bool tmpbool;
+    if(load_loci(prefix_path, sloci, 2, false, tmpbool) != 1) {
+        cerr << "Error: could not find stacks files '" << prefix_path << ".*' (tags, snps and/or alleles).\n";
+        throw exception();
+    }
+
+    // Discard loci that aren't bijective with the catalog
+    for (auto sloc=sloci.begin(); sloc!=sloci.end();) {
+        if (sloc_to_cloc.count(sloc->second->id)) {
+            sloc++;
+        } else {
+            delete sloc->second;
+            sloci.erase(sloc++);
+        }
+    }
+
+    for (CatMatch* m : matches)
+        delete m;
+}
+
+void write_bam_file(const std::map<int, Locus*>& sloci, const std::unordered_map<int, int>& sloc_to_cloc) {
+    //TODO
 }
 
 const string help_string = string() +
