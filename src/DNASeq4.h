ba1f127f5d5d322ca848b7f2d8ee53bedca099bf
Nicolas Rochette <rochette@illinois.edu>
2017-01-19 12:56:13 -0600

    Implemented class DNASeq4; modified DiNuc.

--- src/DNASeq4.h
+++ src/DNASeq4.h
@@ -1,36 +1,89 @@
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+
 #include "hts.h" // For `seq_nt16_table`, `seq_nt16_str`.
 
 #include "constants.h"
 
+class DiNuc;
+class DNASeq4;
+
 // A dinucleotide, coded on one byte (4 bits per nucleotide).
-// 1,2,4,8,15 are A,C,T,G,N like in Htslib, and the high-bits
-// correspond to the first nucleotide.
+// A,C,T,G,N are 1,2,4,8,15 like in Htslib, and the first
+// nucleotide is coded with the high bits.
+// Unlike in Htslib, '\0' (rather than '=') is 0, and other
+// letters are converted to Ns.
 class DiNuc {
     uchar u_;
 
 public:
     DiNuc() : u_(0) {}
     DiNuc(const DiNuc& other) : u_(other.u_) {}
-    DiNuc(char c1, char c2) : u_(0) {set_first(c2u(c1)); set_first(c2u(c2));}
+    DiNuc(char c1, char c2) : u_(0) {set_first(c2u[(uchar) c1]); set_second(c2u[(uchar) c2]);}
 
-    uchar ufirst() {return u_ & lowbits;}
-    uchar usecond() {return u_ >>4;}
+    uchar first() const {return u_ >>4;}
+    uchar second() const {return u_ & 15;}
+    char cfirst() const {return u2c[first()];}
+    char csecond() const {return u2c[second()];}
 
-    char first() {return u2c(ufirst());}
-    char second() {return u2c(usecond());}
-
-    static uchar c2u(char c) {return c2u_table[c];}
-    static char u2c(uchar u) {return u2c_table[u];}
+    DiNuc& operator=(const DiNuc& other) {u_ = other.u_; return *this;}
+    bool operator==(const DiNuc& other) const {return u_ == other.u_;}
+    bool operator<(const DiNuc& other) const {return u_ < other.u_;}
 
 private:
-    static const uchar lowbits = 15;
-    static const uchar highbits = ~15;
-    static const uchar DiNuc::c2u_table[256];
-    static const char DiNuc::u2c_table[16];
+    void set_first(uchar u) {u_ |= u <<4;}
+    void set_second(uchar u) {u_ |= u;}
 
-    void set_first(uchar u) {u_ |= u;}
-    void set_second(uchar u) {u_ |= u <<4;}
+    void clear_first() {u_ &= 15;}
+    void clear_second() {u_ &= ~15;}
 
-    void clear_first() {u_ &= lowbits;}
-    void clear_second() {u_ &= highbits;}
+    // Trivial ASCII-like hash table for characters, adapted from `htslib::seq_nt16_table` (hts.h).
+    // Value is 1 for 'A' and 'a', etc., 0 for '\0' (htslib uses '='), 15 otherwise.
+    static const uchar c2u[256];
+    static const char u2c[16];
+
+public:
+    uchar value() const {return u_;} // for hashing.
 };
+
+class DNASeq4 {
+    std::vector<DiNuc> v_;
+
+public:
+    DNASeq4() : v_() {}
+    DNASeq4(const DNASeq4& other) : v_(other.v_) {}
+    DNASeq4(DNASeq4&& other) : v_(std::move(other.v_)) {}
+    DNASeq4(const char* s, size_t len);
+    DNASeq4(const std::string& s) : DNASeq4(s.c_str(), s.size()) {}
+
+    size_t length() const {return v_.size()*2 - size_t(v_.back().second()==0);}
+    char nuc(size_t i) const {return i%2==0 ? v_[i].cfirst() : v_[i].csecond();}
+
+    size_t vsize() const {return v_.size();}
+    DiNuc operator[](size_t i) const {return v_[i];}
+    const DiNuc* vdata() const {return v_.data();}
+
+    DNASeq4& operator=(const DNASeq4& other) {v_ = other.v_; return *this;}
+    DNASeq4& operator=(DNASeq4&& other) {v_ = std::move(other.v_); return *this;}
+    bool operator==(const DNASeq4& other) const {return v_ == other.v_;}
+    bool operator<(const DNASeq4& other) const {return v_ < other.v_;}
+};
+
+// Specialization for std::hash
+// Based on GCC
+namespace std {
+template<>
+struct hash<DNASeq4> {
+    size_t operator()(const DNASeq4& s) const {
+        size_t x = static_cast<size_t>(14695981039346656037ULL);
+        for (size_t i = 0; i < s.vsize(); i++) {
+            x ^= static_cast<size_t>(s[i].value());
+            x *= static_cast<size_t>(1099511628211ULL);
+        }
+        return x;
+    }
+};
+}
