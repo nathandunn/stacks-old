9ca6dd9704a9749cd446b429f65a6861db9de7e1
Nicolas Rochette <rochette@illinois.edu>
2017-01-20 18:16:10 -0600

    denovo_map.pl: Removed deprecated option -r from the ustacks command line.

--- scripts/denovo_map.pl
+++ scripts/denovo_map.pl
@@ -134,11 +134,11 @@ sub execute_stacks {
         $path = $sample->{'path'} . $sample->{'file'} . "." . $sample->{'suffix'};
         
         if ($sample->{'type'} eq "sample") {
-            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id -r $minc "  . join(" ", @_ustacks) . " 2>&1";
+            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id $minc "  . join(" ", @_ustacks) . " 2>&1";
         } elsif ($sample->{'type'} eq "parent") {
-            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id -r $minc "  . join(" ", @_ustacks) . " 2>&1";
+            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id $minc "  . join(" ", @_ustacks) . " 2>&1";
         } elsif ($sample->{'type'} eq "progeny") {
-            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id -r $minrc " . join(" ", @_ustacks) . " 2>&1";
+            $cmd = $exe_path . "ustacks -t $sample->{'fmt'} -f $path -o $out_path -i $sample_id $minrc " . join(" ", @_ustacks) . " 2>&1";
         }
         print STDERR  "  $cmd\n";
         print $log_fh "$cmd\n";
