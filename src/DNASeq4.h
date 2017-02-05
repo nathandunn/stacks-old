8feeec1802d4d8ba61a6412c1c0263566127614b
Nicolas Rochette <rochette@illinois.edu>
2017-02-04 19:52:15 -0600

    Formatting in DNASeq4.h.

--- src/DNASeq4.h
+++ src/DNASeq4.h
@@ -47,13 +47,13 @@ public:
     DiNuc(uchar val) : u_(val) {}
     DiNuc(uchar u1, uchar u2) : u_(0) {set_first(u1); set_second(u2);}
     DiNuc(char c1, char c2) : DiNuc(nt4::c2u[(int) c1], nt4::c2u[(int) c2]) {}
+    DiNuc& operator= (const DiNuc& other) {u_ = other.u_; return *this;}
 
     uchar first() const {return u_ >>4;}
     uchar second() const {return u_ & 15;}
 
-    DiNuc& operator=(const DiNuc& other) {u_ = other.u_; return *this;}
-    bool operator==(const DiNuc& other) const {return u_ == other.u_;}
-    bool operator<(const DiNuc& other) const {return u_ < other.u_;}
+    bool operator== (const DiNuc& other) const {return u_ == other.u_;}
+    bool operator<  (const DiNuc& other) const {return u_ < other.u_;}
 
 private:
     void set_first(uchar u) {u_ |= u <<4;}
@@ -77,14 +77,14 @@ public:
     DNASeq4(const char* s, size_t len);
     DNASeq4(const uchar* arr, size_t n) : l_(n*2), v_(arr, arr+n) {if (v_.back().second()==0) --l_;}
     DNASeq4(const std::string& s) : DNASeq4(s.c_str(), s.size()) {}
+    DNASeq4& operator= (const DNASeq4& other) {l_ = other.l_; v_ = other.v_; return *this;}
+    DNASeq4& operator= (DNASeq4&& other) {l_ = other.l_; v_ = std::move(other.v_); return *this;}
 
     size_t length() const {return l_;}
-    uchar operator[] (size_t i) const {return i%2==0 ? v_[i/2].first() : v_[i/2].second();}
 
-    DNASeq4& operator=(const DNASeq4& other) {l_ = other.l_; v_ = other.v_; return *this;}
-    DNASeq4& operator=(DNASeq4&& other) {l_ = other.l_; v_ = std::move(other.v_); return *this;}
-    bool operator==(const DNASeq4& other) const {return l_ == other.l_ && v_ == other.v_;}
-    bool operator<(const DNASeq4& other) const {return l_ < other.l_ ? true : v_ < other.v_;}
+    uchar operator[] (size_t i) const {return i%2==0 ? v_[i/2].first() : v_[i/2].second();}
+    bool  operator== (const DNASeq4& other) const {return l_ == other.l_ && v_ == other.v_;}
+    bool  operator<  (const DNASeq4& other) const {return l_ < other.l_ ? true : v_ < other.v_;}
     friend class std::hash<DNASeq4>;
 
     // Iterator.
@@ -94,11 +94,11 @@ public:
 
     public:
         const_iterator(std::vector<DiNuc>::const_iterator vi, bool f) : vi_(vi), first_(f) {}
-        bool operator!=(const_iterator other) {return ! (vi_ == other.vi_? first_ == other.first_ : false);}
-        const_iterator& operator++() {if (first_) {first_ = false;} else {++vi_; first_ = true;} return *this; }
+        bool operator!= (const_iterator other) {return ! (vi_ == other.vi_? first_ == other.first_ : false);}
+        const_iterator& operator++ () {if (first_) {first_ = false;} else {++vi_; first_ = true;} return *this; }
 
         // Get the nucleotide.
-        uchar operator*() {return first_ ? vi_->first() : vi_->second();}
+        uchar operator* () {return first_ ? vi_->first() : vi_->second();}
     };
     const_iterator begin() const {return const_iterator(v_.begin(), true);}
     const_iterator end()   const {return length()%2==0 ? const_iterator(v_.end(), true) : const_iterator(--v_.end(), false);}
@@ -111,7 +111,7 @@ public:
 namespace std {
 template<>
 struct hash<DNASeq4> {
-    size_t operator()(const DNASeq4& s) const {
+    size_t operator() (const DNASeq4& s) const {
         size_t x = static_cast<size_t>(14695981039346656037ULL);
         for (const DiNuc& d : s.v_) {
             x ^= static_cast<size_t>(d.u_);
