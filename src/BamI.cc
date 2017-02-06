d868f4b2838856c69722f28a429b32ad92ff94c0
Nicolas Rochette <rochette@illinois.edu>
2017-02-06 17:31:10 -0600

    Fleshed up assemble_pe.cc; added read_one_locus() and BamHeader::read_groups().

--- src/BamI.cc
+++ src/BamI.cc
@@ -1,4 +1,6 @@
 #include <cstring>
+#include <string>
+#include <map>
 
 #include "constants.h"
 #include "BamI.h"
@@ -24,4 +26,61 @@ void write_bam_header(
 
     // Clean up.
     bam_hdr_destroy(hdr);
+}
+
+BamHeader::ReadGroups BamHeader::read_groups() const {
+    ReadGroups rgs;
+
+    // xxx (Feb2017) I currently store the sample ID in the read group ID, but
+    // BAM read group IDs are unstable. It would be better to use a custom field
+    // such as 'si' (sample id).
+
+    const char* end = h_-> text + h_->l_text;
+    const char* p = h_->text;
+    while (*p) {
+        // Process a line.
+
+        if (p + 3 < end && strncmp(p, "@RG\t", 4) == 0) {
+            // This is a @RG line.
+            map<string, string> rg;
+
+            p+=4;
+            while (*p && *p != '\n') {
+                // Process a tag.
+
+                string tag;
+                while (*p && *p != '\n' && *p != '\t' && *p != ':') {
+                    tag += *p;
+                    ++p;
+                }
+                if (*p == ':')
+                    ++p;
+
+                string value;
+                while (*p && *p != '\n' && *p != '\t') {
+                    value += *p;
+                    ++p;
+                }
+
+                if (tag.size() == 2 && !value.empty())
+                    rg.insert({tag, value});
+
+                if (*p == '\t')
+                    ++p;
+            }
+
+            try {
+                string id = rg.at("ID"); // might throw
+                rgs.insert({move(id), move(rg)});
+            } catch (out_of_range&) {} // ignore
+        }
+
+        while(*p && *p != '\n')
+            ++p;
+
+        if (*p == '\n')
+            ++p;
+    }
+
+    return rgs;
 }
