e8969e8b988317b5d2670755f9afc924b98c9674
Nicolas Rochette <rochette@illinois.edu>
2017-02-12 19:21:30 -0600

    ctg.cc: Implemented assembly, relying on GATB's Graph and Simplifications classes.

--- src/ctg.cc
+++ src/ctg.cc
@@ -1,55 +1,103 @@
+#include <vector>
 #include <string>
 
+#define WITH_LAMBDA_EXPRESSION
 #include <gatb/gatb_core.hpp>
+
+#include <gatb/debruijn/impl/Simplifications.hpp>
+#include <gatb/debruijn/impl/GraphUnitigs.hpp>
 
 using namespace std;
 
+const string reads_path = "reads.fq.gz";
+const string out_path = "out.fa";
+const size_t min_cov = 2;
+const size_t klen = 20;
+const bool remove_tips = false;
+const bool remove_bulges = false;
+const bool remove_ec = false;
+
+struct Contig {
+    NodeGU starting_node;
+    string seq;
+    float cov;
+    bool isolated_left;
+    bool isolated_right;
+};
+
+//
+// Classes to use with Integer::apply<Functor, Params>(size_t klen, Params params);
+//
+
+struct AssemblyArgs {
+    const vector<string>& reads;
+    size_t klen;
+    size_t min_cov;
+    vector<Contig>& contigs;
+};
+
+template<size_t span>
+struct AssemblyFunctor {
+    void operator() (AssemblyArgs& p);
+};
+
+//
+// main
+//
+
 int main(int argc, char* argv [])
 {
-    // TODO Initialize graph?
-
-    // Read the input sequences.
+    // Read the sequences into a vector<string>
+    vector<string> reads;
     {
-        const string reads_path = "./reads.fq.gz";
-        IBank * inbank = Bank::open(reads_path);
-        Iterator<Sequence>* it = inbank->iterator();
-        for (it->first(); !it->isDone(); it->next()) {
-            Sequence& seq = it->item();
-            // Print the sequence.
-            cout << " [ " << seq.getDataSize() << " ] " << seq.getComment() << endl;
-            cout << seq.toString() << endl ;
-
-            // TODO Build graph?
-        }
-        delete it;
-        delete inbank;
+        IBank* ibank = Bank::open (reads_path);
+        Iterator<Sequence>* s = ibank->iterator();
+        for(s->first(); !s->isDone(); s->next())
+            reads.push_back(s->item().toString());
     }
 
-    // TODO Process graph?
+    // Do the assembly
+    vector<Contig> contigs;
+    AssemblyArgs args = {reads, klen, min_cov, contigs};
+    Integer::apply<AssemblyFunctor, AssemblyArgs>(args.klen, args);
+
+    // Write the resulting contigs
+    cout << "args.contigs->size(): " << args.contigs.size() << endl;
+    ofstream ofile (out_path);
+    ofile << fixed << setprecision(2);
+    for(size_t i=0; i<args.contigs.size(); ++i) {
+        Contig& c = args.contigs.at(i);
+        ofile << ">NODE_"<< i+1 << "_length_" << c.seq.size() << "_cov_" << c.cov << "_ID_" << i << "\n"
+              << c.seq << "\n";
+    }
 }
 
-/*
- * // More code examples -- c.f. Tutorial "GATB Programming Day"
- *
- * // Writing sequence files.
- * IBank* outBank = new BankFasta("path") ;
- * for (...) {Sequence& seq = ...; outBank->insert(seq);}
- * outBank->flush();
- *
- * // Manipulating Kmers.
- * // (Also `KmerCanonical`.)
- * Kmer<span>::ModelDirect model (kmerSize);
- * Kmer<span>::ModelCanonical modelc (kmerSize);
- * Kmer <span>::ModelDirect::Iterator itKmer (model);
- * itKmer->value() // forward(), revcomp()
- *
- * // Graph creation: Graph::create or Graph::load, e.g.
- * Graph graph = Graph::create("-in %s -abundance-min %d -kmer-size %d -debloom original", reads_path, 3, kmerSize);
- *
- * // Iterating over nodes -- c.f. "Overview"
- *
- * Graph::Iterator<Node> it = graph.iterator<Node>();
- * for (it.first(); !it.isDone(); it.next()) {
- *     std::cout << graph.toString (it.item()) << std::endl;
- * }
- */
+template<size_t span>
+void AssemblyFunctor<span>::operator() (AssemblyArgs& args) {
+    typedef GraphUnitigsTemplate<span> Graph;
+
+    // Initialize the graph
+    BankStrings* bank = new BankStrings(args.reads);
+    Graph graph = Graph::create(bank, "-kmer-size %d -abundance-min %d", args.klen, args.min_cov);
+    graph.precomputeAdjacency();
+
+    // Perform simplifications
+    Simplifications<Graph,NodeGU,EdgeGU> simplifications(graph, 1, false); // one core, quiet
+    simplifications._doTipRemoval = remove_tips;
+    simplifications._doBulgeRemoval = remove_bulges;
+    simplifications._doECRemoval = remove_ec;
+    simplifications.simplify();
+
+    // Retrieve the resulting contigs
+    GraphIterator<NodeGU> node_it = graph.iterator();
+    for(node_it.first(); !node_it.isDone(); node_it.next()) {
+        NodeGU node = node_it.item();
+        if (graph.unitigIsMarked(node) || graph.isNodeDeleted(node))
+            continue;
+
+        args.contigs.push_back(Contig());
+        Contig& c = args.contigs.back();
+        c.starting_node = node;
+        c.seq = graph.simplePathBothDirections(c.starting_node, c.isolated_left, c.isolated_right, true, c.cov);
+    }
+}
