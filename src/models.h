cc02461bf735814ad85cbaa1f045ed4c43bcab6b
Nicolas Rochette <rochette@illinois.edu>
2017-03-02 16:18:33 -0600

    models.h/cc: Reorganized the code.

--- src/models.h
+++ src/models.h
@@ -51,32 +51,120 @@ extern double bound_high;
 extern double heterozygote_limit;
 extern double homozygote_limit;
 
-snp_type call_bounded_multinomial_snp(MergedStack *, int, map<char, int> &, bool);
-snp_type call_bounded_multinomial_snp(Locus *, int, map<char, int> &);
-snp_type call_multinomial_snp(MergedStack *, int, map<char, int> &, bool);
-snp_type call_multinomial_snp(Locus *, int, map<char, int> &);
-int      call_multinomial_fixed(MergedStack *, int, map<char, int> &);
+void     set_model_thresholds (double alpha);
+snp_type call_snp (double l_ratio);
+
+double   multinomial_model         (double nuc_1, double nuc_2, double nuc_3, double nuc_4);
+double   bounded_multinomial_model (double nuc_1, double nuc_2, double nuc_3, double nuc_4);
+
+void call_bounded_multinomial_snp(MergedStack *, int, map<char, int> &, bool);
+void call_bounded_multinomial_snp(Locus *, int, map<char, int> &);
+void call_multinomial_snp(MergedStack *, int, map<char, int> &, bool);
+void call_multinomial_snp(Locus *, int, map<char, int> &);
+void call_multinomial_fixed(MergedStack *, int, map<char, int> &);
+
 double   heterozygous_likelihood(int, map<char, int> &);
 double   homozygous_likelihood(int, map<char, int> &);
 
+//
+// ==================
+// Inline definitions
+// ==================
+//
+
 inline
-void set_model_thresholds(double alpha) {
-    if (alpha == 0.1) {
-        heterozygote_limit = -2.71;
-        homozygote_limit   =  2.71;
-    } else if (alpha == 0.05) {
-        heterozygote_limit = -3.84;
-        homozygote_limit   =  3.84;
-    } else if (alpha == 0.01) {
-        heterozygote_limit = -6.64;
-        homozygote_limit   =  6.64;
-    } else if (alpha == 0.001) {
-        heterozygote_limit = -10.83;
-        homozygote_limit   =  10.83;
-    } else {
-        cerr << "Error: Unsupported alpha value '" << alpha << "'.\n";
-        throw exception();
-    }
+snp_type call_snp (double l_ratio) {
+    if (l_ratio <= heterozygote_limit)
+        return snp_type_het;
+    else if (l_ratio >= homozygote_limit)
+        return snp_type_hom;
+    else
+        return snp_type_unk;
+}
+
+inline
+double lr_multinomial_model (double nuc_1, double nuc_2, double nuc_3, double nuc_4) {
+
+    //
+    // Method of Paul Hohenlohe <hohenlohe@uidaho.edu>, personal communication.
+    //
+    // For a diploid individual, there are ten possible genotypes
+    // (four homozygous and six heterozygous genotypes).  We calculate
+    // the likelihood of each possible genotype by using a multinomial
+    // sampling distribution, which gives the probability of observing
+    // a set of read counts (n1,n2,n3,n4) given a particular genotype.
+    //
+
+    double total = nuc_1 + nuc_2 + nuc_3 + nuc_4;
+    assert(total > 0.);
+
+    double l_ratio = (nuc_1 * log(nuc_1 / total));
+
+    if (total - nuc_1 > 0.)
+        l_ratio += ((total - nuc_1) * log((total - nuc_1) / (3. * total)));
+
+    if (nuc_1 + nuc_2 > 0.)
+        l_ratio -= ((nuc_1 + nuc_2) * log((nuc_1 + nuc_2) / (2. * total)));
+
+    if (nuc_3 + nuc_4 > 0.)
+        l_ratio -= ((nuc_3 + nuc_4) * log((nuc_3 + nuc_4) / (2. * total)));
+
+    l_ratio *= 2.;
+
+    return l_ratio;
+}
+
+inline
+double lr_bounded_multinomial_model (double nuc_1, double nuc_2, double nuc_3, double nuc_4) {
+
+    //
+    // Method of Paul Hohenlohe <hohenlohe@uidaho.edu>, personal communication.
+    //
+
+    double total = nuc_1 + nuc_2 + nuc_3 + nuc_4;
+    assert(total > 0.);
+
+    //
+    // Calculate the site specific error rate for homozygous and heterozygous genotypes.
+    //
+    double epsilon_hom  = (4. / 3.) * ((total - nuc_1) / total);
+    double epsilon_het  = 2. * ((nuc_3 + nuc_4) / total);
+
+    //
+    // Check if the error rate is above or below the specified bound.
+    //
+    if (epsilon_hom < bound_low)
+        epsilon_hom = bound_low;
+    else if (epsilon_hom > bound_high)
+        epsilon_hom = bound_high;
+
+    if (epsilon_het < bound_low)
+        epsilon_het = bound_low;
+    else if (epsilon_het > bound_high)
+        epsilon_het = bound_high;
+
+    //
+    // Calculate the log likelihood for the homozygous and heterozygous genotypes.
+    //
+    double ln_L_hom = nuc_1 * log(1 - ((3./4.) * epsilon_hom));
+    ln_L_hom += epsilon_hom > 0. ? ((nuc_2 + nuc_3 + nuc_4) * log(epsilon_hom / 4.)) : 0.;
+
+    double ln_L_het = (nuc_1 + nuc_2) * log(0.5 - (epsilon_het / 4.));
+    ln_L_het += epsilon_het > 0. ? ((nuc_3 + nuc_4) * log(epsilon_het / 4.)) : 0.;
+
+    //
+    // Calculate the likelihood ratio.
+    //
+    double l_ratio  = 2. * (ln_L_hom - ln_L_het);
+
+    // cerr << "  Nuc_1: " << nuc_1 << " Nuc_2: " << nuc_2 << " Nuc_3: " << nuc_3 << " Nuc_4: " << nuc_4
+    //   << " epsilon homozygote: " << epsilon_hom
+    //   << " epsilon heterozygote: " << epsilon_het
+    //   << " Log likelihood hom: " << ln_L_hom
+    //   << " Log likelihood het: " << ln_L_het
+    //   << " Likelihood ratio: " << l_ratio << "\n";
+
+    return l_ratio;
 }
 
 #endif // __MODELS_H__
