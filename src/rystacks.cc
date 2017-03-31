3e7ce61ad04c087be676e76fa7713160411ea311
Nicolas Rochette <rochette@illinois.edu>
2017-03-31 17:35:25 -0500

    New program rystacks (largely based on assemble_pe).

--- src/rystacks.cc
+++ src/rystacks.cc
@@ -1,10 +1,8 @@
-#include <string>
-
 #include <getopt.h>
 
 #include "constants.h"
 #include "log_utils.h"
-#include "MetaPopInfo.h"
+#include "catalog_utils.h"
 #include "locus.h"
 #include "BamCLocReader.h"
 #include "debruijn.h"
@@ -14,15 +12,21 @@
 
 using namespace std;
 
+void parse_command_line(int argc, char* argv[]);
+void report_options(ostream& os);
+void process_one_locus(CLocReadSet&& loc);
+
+const string prog_name = "rystacks";
+
 //
 // Argument globs.
 //
 bool quiet = false;
-string bam_path;
-string out_dir;
-size_t km_length = -1;
-size_t min_km_count = 2;
+string in_dir;
+int batch_id = -1;
 set<int> locus_wl;
+size_t km_length = 31;
+size_t min_km_count = 2;
 bool fasta_out = false;
 bool gfa_out = false;
 bool aln_out = false;
@@ -32,21 +36,13 @@ bool aln_out = false;
 //
 LogAlterator* lg = NULL;
 
-//
-// Function declarations.
-//
-void process_one_locus(CLocReadSet&& loc, Graph& graph);
-
-void parse_command_line(int argc, char* argv[]);
-void report_options(ostream& os);
-
 int main(int argc, char** argv) {
 
     // Parse arguments.
     parse_command_line(argc, argv);
 
     // Open the log.
-    string lg_path = out_dir + "assemble_pe.log";
+    string lg_path = in_dir + prog_name + ".log";
     if(!quiet)
         cout << "Logging to '" << lg_path << "'." << endl;
     lg = new LogAlterator(lg_path, quiet);
@@ -55,24 +51,23 @@ int main(int argc, char** argv) {
     cout << "\n" << flush;
 
     // Open the BAM file and parse the header.
-    BamCLocReader bam_fh (bam_path);
+    BamCLocReader bam_fh (in_dir + "batch_" + to_string(batch_id) + ".catalog.bam");
     const MetaPopInfo& mpopi = bam_fh.mpopi();
 
     // Process every locus
     CLocReadSet loc (mpopi);
-    Graph graph (km_length);
     size_t n_loci = 0;
     if (locus_wl.empty()) {
         // No whitelist.
         while (bam_fh.read_one_locus(loc)) {
-            process_one_locus(move(loc), graph);
+            process_one_locus(move(loc));
             ++n_loci;
         }
 
     } else {
         while (bam_fh.read_one_locus(loc) && !locus_wl.empty()) {
             if (locus_wl.count(loc.id())) {
-                process_one_locus(move(loc), graph);
+                process_one_locus(move(loc));
                 ++n_loci;
                 locus_wl.erase(loc.id());
             }
@@ -84,22 +79,19 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void process_one_locus(CLocReadSet&& loc, Graph& graph) {
-    if (fasta_out) {
-        ofstream fasta (out_dir + to_string(loc.id()) + ".fa");
-        for (const Read& r : loc.reads())
-            fasta << ">" << r.name << "\n" << r.seq.str() << "\n";
-    }
+void process_one_locus(CLocReadSet&& loc) {
+    assert(!loc.reads().empty());
 
     //
     // Assemble the reads.
     //
+    Graph graph (km_length);
     graph.rebuild(loc, min_km_count);
     if (graph.empty())
         return;
 
     if (gfa_out)
-        graph.dump_gfa(out_dir + to_string(loc.id()) + ".gfa");
+        graph.dump_gfa(in_dir + to_string(loc.id()) + ".gfa");
 
     vector<const SPath*> best_path;
     if (!graph.find_best_path(best_path))
@@ -107,6 +99,11 @@ void process_one_locus(CLocReadSet&& loc, Graph& graph) {
         return;
 
     string ctg = SPath::contig_str(best_path.begin(), best_path.end(), km_length);
+
+    //
+    // Get the contig of the locus.
+    //
+    ctg = loc.reads().at(0).seq.str() + string(10, 'N') + ctg; //xxx
 
     //
     // Align each read to the contig.
@@ -129,46 +126,44 @@ void process_one_locus(CLocReadSet&& loc, Graph& graph) {
     }
 
     if (aln_out) {
-        ofstream aln (out_dir + to_string(loc.id()) + ".aln");
+        ofstream aln (in_dir + to_string(loc.id()) + ".aln");
         aln << aln_loc << "\n";
     }
-
 }
 
 const string help_string = string() +
-        "assemble_pe " + VERSION  + "\n"
-        "assemble_pe -b bam_file -O out_dir -k km_len\n"
+        prog_name + VERSION  + "\n" +
+        prog_name + " -P in_dir\n"
         "\n"
-        "  -b: path to the input sorted read, in BAM format\n"
-        "  -O: path to an output directory\n"
-        "  -k: kmer length\n"
-        "  --min-cov: minimum coverage to consider a kmer (default 2)\n"
+        "  -P: input directory (must contain a batch_X.catalog.bam file)\n"
+        "  -b: batch ID (default: guess)\n"
         "  -W,--whitelist: a whitelist of locus IDs\n"
-        "  --fasta: output a Fasta file for each locus\n"
+        "\n"
+        "Alignment options:\n"
+        "  -k: kmer length (default: 31)\n"
+        "  --min-cov: minimum coverage to consider a kmer (default: 2)\n"
         "  --gfa: output a GFA file for each locus\n"
-        "  --aln: output a file showing the contig & read alignments\n"
+        "  --aln: output a file showing the contig & read alignments for each locus\n"
         "\n"
         ;
 
-void bad_args() {
-    cerr << help_string;
-    exit(13);
-}
-
 void parse_command_line(int argc, char* argv[]) {
 
+    auto bad_args = [](){
+        cerr << help_string;
+        exit(13);
+    };
+
     static const option long_options[] = {
-        {"help",         no_argument,       NULL, 'h'},
-        {"quiet",        no_argument,       NULL, 'q'},
         {"version",      no_argument,       NULL,  1000},
-        {"bam-file",     required_argument, NULL, 'b'},
-        {"out-dir",      required_argument, NULL, 'O'},
-        {"kmer-length",  required_argument, NULL, 'k'},
-        {"min-cov",      required_argument, NULL,  1003},
+        {"help",         no_argument,       NULL,  'h'},
+        {"quiet",        no_argument,       NULL,  'q'},
+        {"in-dir",       required_argument, NULL,  'P'},
         {"whitelist",    required_argument, NULL,  'W'},
-        {"gfa",          no_argument,       NULL,  1004},
-        {"fasta",        no_argument,       NULL,  1002},
-        {"aln",          no_argument,       NULL,  1005},
+        {"kmer-length",  required_argument, NULL,  1001},
+        {"min-cov",      required_argument, NULL,  1002},
+        {"gfa",          no_argument,       NULL,  1003},
+        {"aln",          no_argument,       NULL,  1004},
         {0, 0, 0, 0}
     };
 
@@ -184,6 +179,10 @@ void parse_command_line(int argc, char* argv[]) {
             break;
 
         switch (c) {
+        case 1000: //version
+            cout << "pstacks_pe " << VERSION << "\n";
+            exit(0);
+            break;
         case 'h':
             cout << help_string;
             exit(0);
@@ -191,32 +190,25 @@ void parse_command_line(int argc, char* argv[]) {
         case 'q':
             quiet = true;
             break;
-        case 1000: //version
-            cout << "pstacks_pe " << VERSION << "\n";
-            exit(0);
+        case 'P':
+            in_dir = optarg;
             break;
         case 'b':
-            bam_path = optarg;
-            break;
-        case 'O':
-            out_dir = optarg;
-            break;
-        case 'k':
-            km_length = atoi(optarg);
-            break;
-        case 1003://min-cov
-            min_km_count = atoi(optarg);
+            batch_id = atoi(optarg);
             break;
         case 'W':
             wl_path = optarg;
             break;
-        case 1004://gfa
+        case 1001://kmer-length
+            km_length = atoi(optarg);
+            break;
+        case 1002://min-cov
+            min_km_count = atoi(optarg);
+            break;
+        case 1003://gfa
             gfa_out = true;
             break;
-        case 1002://fasta
-            fasta_out = true;
-            break;
-        case 1005://aln
+        case 1004://aln
             aln_out = true;
             break;
         case '?':
@@ -230,22 +222,27 @@ void parse_command_line(int argc, char* argv[]) {
         cerr << "Error: Failed to parse command line: '" << argv[optind] << "' is seen as a positional argument. Expected no positional arguments.\n";
         bad_args();
     }
-    if (bam_path.empty()) {
-        cerr << "Error: An input BAM file must be provided (-b).\n";
-        bad_args();
-    }
-    if (out_dir.empty()) {
+    if (in_dir.empty()) {
         cerr << "Error: An output directory must be provided (-O).\n";
-        bad_args();
-    }
-    if (km_length == size_t(-1)) {
-        cerr << "Error: A kmer length must be provided (-k).\n";
         bad_args();
     }
 
     // Process arguments.
-    if (out_dir.back() != '/')
-        out_dir += '/';
+    if (in_dir.back() != '/')
+        in_dir += '/';
+
+    if (batch_id < 0) {
+        vector<int> cat_ids = find_catalogs(in_dir);
+        if (cat_ids.size() == 1) {
+            batch_id = cat_ids[0];
+        } else if (cat_ids.empty()) {
+            cerr << "Error: Unable to find a catalog in '" << in_dir << "'.\n";
+            bad_args();
+        } else {
+            cerr << "Error: Input directory contains several catalogs, please specify -b.\n";
+            bad_args();
+        }
+    }
 
     if (!wl_path.empty()) {
         ifstream wl_fh (wl_path);
@@ -265,12 +262,13 @@ void parse_command_line(int argc, char* argv[]) {
 
 void report_options(ostream& os) {
     os << "Configuration for this run:\n"
-       << "  Sorted reads file: '" << bam_path << "'\n"
-       << "  Output directory: '" << out_dir << "'\n"
-       << "  Kmer length: " << km_length << "\n"
-       << "  Min coverage: " << min_km_count << "\n"
+       << "  Input directory: '" << in_dir << "'\n"
+       << "  Batch ID: " << batch_id << "\n"
        ;
-
     if (!locus_wl.empty())
         os << "  Whitelist of " << locus_wl.size() << " loci.\n";
+    if (km_length != 31)
+        os << "  Kmer length: " << km_length << "\n";
+    if (min_km_count != 2)
+        os << "  Min coverage: " << min_km_count << "\n";
 }
