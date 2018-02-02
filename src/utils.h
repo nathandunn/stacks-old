295ad7b19bb1783e2e6ecc6ac8d9f819c16c89b4
Nicolas Rochette <rochette@illinois.edu>
2018-02-02 12:18:10 -0600

    Fixed Timer class.

--- src/utils.h
+++ src/utils.h
@@ -203,8 +203,9 @@ class Timer {
 public:
     Timer() : elapsed_(0.0), consumed_(0.0), start_(0.0) {restart();}
     void   restart() {start_=gettm(); consumed_+=2.0*(gettm()-start_);}
-    double elapsed() {double now=gettm(); elapsed_+=now-start_; start_=now; consumed_+=2.0*(gettm()-now); return elapsed_;}
-    void   stop()    {elapsed();}
+    void   update()  {double now=gettm(); elapsed_+=now-start_; start_=now; consumed_+=2.0*(gettm()-now);}
+    void   stop()    {update();}
+    double elapsed() const {return elapsed_;}
     double consumed() const {return consumed_;}
 
     Timer& operator+=(const Timer& other) {elapsed_+=other.elapsed_; consumed_+=other.consumed_; return *this;}
